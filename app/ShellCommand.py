0c2972f3e025eab2e1a413a4bade538f1aa654b0
celiabd <clia.abd29@gmail.com>
2024-05-22 16:12:06 +0200

    ajout

--- app/ShellCommand.py
+++ app/ShellCommand.py
@@ -15,28 +15,28 @@ class ShellComands:
     #, stdout=subprocess.PIPE, stderr=subprocess.PIPE
     def Normalization(self,inputFile):
         
-        command = f"cd /home/drys/Public/imports-magmi/; php console.php normalize-images data/{inputFile} data/{inputFile}_nrm"  
+        command = f"cd /home/celia/Public/imports-magmi/; php console.php normalize-images data/{inputFile} data/{inputFile}_nrm"  
 
         self.getCommand(command)
         
     def AddImages(self, exportFile, resultFile, inputFile, outputFile):
-        command = f"cd /home/drys/Public/imports-magmi/; php console.php add-images data/{exportFile} data/{resultFile} data/{inputFile}/ data/{outputFile}/"
+        command = f"cd /home/celia/Public/imports-magmi/; php console.php add-images data/{exportFile} data/{resultFile} data/{inputFile}/ data/{outputFile}/"
         
         self.getCommand(command)
         
     
     def ExtractImages(self, jsonFile, outputFile):
-        command = f"cd /home/drys/Public/imports-magmi/; php console.php extract-images data/{jsonFile} data/{outputFile}"
+        command = f"cd /home/celia/Public/imports-magmi/; php console.php extract-images data/{jsonFile} data/{outputFile}"
         
         self.getCommand(command)
         
     def SendImagesInServer(self, inputFile):
-        command = f"rsync -a --progress /home/drys/Public/imports-magmi/data/{inputFile}/ app@10.10.0.227:/mnt/images_magmis/{inputFile}"
+        command = f"rsync -a --progress /home/celia/Public/imports-magmi/data/{inputFile}/ app@10.10.0.227:/mnt/images_magmis/{inputFile}"
         
         self.getCommand(command)
         
     def DownloaderImages(self, inputFile, outputFile):
-        command = f"cd /home/drys/Public/imports-magmi/; php console.php download:images data/{inputFile} data/{outputFile} -s b2b -vvv"
+        command = f"cd /home/celia/Public/imports-magmi/; php console.php download:images data/{inputFile} data/{outputFile} -s b2b -vvv"
         
         self.getCommand(command)
         
